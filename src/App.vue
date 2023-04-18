aba94ed3666959315af77f17dc6db080bede4431
FERNANDOII <jimenezfs@crece.uss.edu.pe>
2023-04-18 07:17:11 -0500

    global pinia commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+   <h3>{{ store.count}} x 2 = {{ store.times2 }}</h3>
+   <button @click="store.Increment(1)">Increment</button>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="ts" setup>
+import { useCounterStore} from './store/counter';
+const store = useCounterStore()
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
